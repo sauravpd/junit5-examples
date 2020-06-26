0eb98a7f4843ad71f6d36d249ca24dbbe0639e5d
saurav <sauravpd58@gmail.com>
2020-06-27 03:07:25 +0530

    added @DisplayName example

---
+++ src/test/java/TestClass4.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestClass4 
+{
+	@Test
+	@DisplayName("TestClass4 test 1")
+	public void test1()
+	{
+		System.out.println("test1");
+	}
+}
