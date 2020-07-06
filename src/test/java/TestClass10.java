2b207bec50d7662425cef2533f4456d9d0fbc066
saurav <test@abcd.com>
2020-07-07 03:30:58 +0530

    added @Tag(smoke) example

---
+++ src/test/java/TestClass10.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class TestClass10 
+{
+	@Test
+	@Tag("smoke")
+	public void test1() {
+		System.out.println("TestClass10: test1");
+	}
+
+	@Test
+	@Tag("regression")
+	public void test2() {
+		System.out.println("TestClass10: test2");
+	}
+
+	@Test
+	@Tag("smoke")
+	public void test3() {
+		System.out.println("TestClass10: test3");
+	}
+
+	@Test
+	@Tag("regression")
+	public void test4() {
+		System.out.println("TestClass10: test4");
+	}
+}
